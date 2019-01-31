1b7866447933900a6c7dfa6ce3267610d1d3388a
matian <mt135246@163.com>
2019-02-01 02:25:13 +0800

    开发登录页面
    完成登录页面的展示

--- matianv1/src/router/index.js
+++ matianv1/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Login  from '@/components/login/login.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+
     }
   ]
 })
